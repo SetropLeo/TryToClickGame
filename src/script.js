7b546fb6bf1476b571920e17924d976015018ef9
LeonardoPortes <leol_portes@hotmail.com>
2021-02-22 22:21:26 -0300

    feat(Finished)

--- src/script.js
+++ src/script.js
@@ -1,69 +1,36 @@
 /*CÓDIGO EM JAVASCRIPT*/
 
-// const handleClick = () => {
-//   const body = document.querySelector(
-//     "body"
-//   ); /*utilizado p selecionar elemento no JS*/
-//   body.innerHTML = `
-//         <h1>Hello JavaScript! </h1>
-//         <h2> and Hello World! </h2>
-//     `;
-
-//   body.style.backgroundColor = "#000";
-//   body.style.color = "#fff";
-// };
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const el = document.getElementById("btn-start");
-//   el.addEventListener("mouseover", () => {
-//     el.style.position = "absolute";
-//     el.style.top = `${Math.floor(Math.random() * (window.innerHeight - 18))}px`;
-//     el.style.left = `${Math.floor(Math.random() * (window.innerWidth - 36))}px`;
-//   });
-//   el.addEventListener("click", () => handleClick());
-// });
-
- 
-/*CÓDIGO EM JQUERY*/
-
 const handleClick = () => {
-  const body = $("body");
-  body.html(`
-          <h1>Hello JavaScript! </h1>
-          <h2> and Hello World! </h2>
-      `);
+  const body = document.querySelector(
+    "body"
+  ); /*utilizado p selecionar elemento no JS*/
+  body.innerHTML = `
+        <h1>Hello JavaScript! </h1>
+        <h2> and Hello World! </h2>
+    `;
 
-  body.css("background-color", "#000");
-  body.css("color", "#FFF");
+  body.style.backgroundColor = "#000"
+  body.style.color = "#fff"
 };
 
-const startButtonInit = () => {
-  const el = $("#btn-start");
-  el.on("mouseover", () => {
-    el.css("position", "absolute"),
-      el.css(
-        "top",
-        `${Math.floor(Math.random() * (window.innerHeight - 18))}px`
-      );
-    el.css("left", `${Math.floor(Math.random() * (window.innerWidth - 36))}px`);
-  });
-  el.on("click", () => handleClick());
-};
-
-const infoButtonInit = () => {
-  const el = $("#btn-info");
-  const elInitialText = el.html();
-  el.on("click", () => {
-    if (el.html() == elInitialText) {
-      el.html("HIDE INFOS");
-    } else {
-      el.html(elInitialText);
-    }
-    $(".info").slideToggle();
-  });
-};
-
-$(document).ready(() => {
-  startButtonInit();
-  infoButtonInit();
+const el = document.getElementById('btn-start');
+el.addEventListener('mouseover', () => {
+  el.style.position = 'absolute';
+  el.style.top = `${Math.floor(Math.random() * (window.innerHeight - 18))}px`;
+  el.style.left = `${Math.floor(Math.random() * (window.innerWidth - 36))}px`;
 });
+el.addEventListener('click', handleClick);
+
+
+const btnInfo = document.getElementById('btn-info')
+const list = document.getElementById('ul')
+
+const homeInfo = () => {
+  list.classList.toggle('remove')
+  if(list.classList.contains('remove')){
+    btnInfo.innerText= 'Hide Infos'
+  }else{
+    btnInfo.innerText= 'Show Infos'
+  }
+}
+btnInfo.addEventListener('click', homeInfo)
